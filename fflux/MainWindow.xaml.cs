bc085178206128fc2637524e0b8b3f499723053f
LimBaehyeon <ImFlower@outlook.com>
2026-05-05 23:37:13 +0900

    add package

--- fflux/MainWindow.xaml.cs
+++ fflux/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wpf.Ui.Appearance;
 
 namespace fflux
 {
@@ -19,6 +20,7 @@ namespace fflux
         public MainWindow()
         {
             InitializeComponent();
+            ApplicationThemeManager.Apply(this);
         }
     }
 }
